e6a56c06dce004100758ae24eb28cbd6794614f0
mbrie041 <mbrie041@gmail.com>
2021-09-06 11:43:45 -0400

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  username = "";
+  onUpdateUsername(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+  // resetUsername() {
+  //   this.username = "";
+  // }
 }
